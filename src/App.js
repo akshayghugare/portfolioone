38e39d596c4c34c38eeda34d105904d1a90176aa
AkshayGhugare19 <114797466+AkshayGhugare19@users.noreply.github.com>
2024-06-04 16:34:46 +0530

    tailwind add and app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { FaSun, FaMoon } from 'react-icons/fa';
+import Sidebar from './components/Sidebar';
+import IndexRoute from './Routes/IndexRoute';
 
-function App() {
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${isDarkMode ? 'dark' : ''}`}>
+      <Router>
+        <div className="flex h-screen">
+          <Sidebar />
+          <div className="flex-grow  bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white">
+          <button
+              onClick={toggleTheme}
+              className="mb-5 ml-2 px-4 py-2 rounded bg-blue-500 text-white fixed top-3 right-1 mt-1 flex items-center justify-center"
+              aria-label="Toggle Theme"
+            >
+              {isDarkMode ? <FaSun size={24} /> : <FaMoon size={24} />}
+            </button>
+            <Routes>
+              <Route path="/*" element={<IndexRoute />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
